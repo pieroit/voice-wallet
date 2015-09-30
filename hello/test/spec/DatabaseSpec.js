2cc226a6bfcb19cd787543033df86c03bdd2bb34
pieroit <savastanos@libero.it>
2015-09-30 17:08:09 +0200

    before refactoring DB

---
+++ hello/test/spec/DatabaseSpec.js
@@ -0,0 +1,27 @@
+
+describe('Database functions', function(){
+
+    var db = new Database();
+
+    it('create db correclty', function(){
+        
+    });
+    
+    // Should be a form method... not DB
+    it('validate record', function(){
+
+    });
+    
+    it('insert record', function(){
+        
+    });
+    
+    it('upsert record', function(){
+        
+    });
+    
+    it('get records', function(){
+            
+    });
+
+});
